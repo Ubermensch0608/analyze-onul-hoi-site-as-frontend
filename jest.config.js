b6b3cd61bc0ff6f10509c24d6d961624f38fd371
JongWon <melancholyee28@gmail.com>
2022-08-22 04:36:03 +0900

    Add: GNB 메뉴 - 테스트 및 기능 구현

--- jest.config.js
+++ jest.config.js
@@ -34,6 +34,7 @@ module.exports = {
 		// Use babel-jest to transpile tests with the next/babel preset
 		// https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
 		'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+		'^.+\\.svg$': '<rootDir>/__mocks__/svgTransformer.js',
 	},
 	transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
